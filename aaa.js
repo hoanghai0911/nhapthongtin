4625760346c06c04383273976746c66d8555de5a
hoanghai0911 <102914224+hoanghai0911@users.noreply.github.com>
2022-04-04 11:14:44 +0700

    Add files via upload

---
+++ aaa.js
@@ -0,0 +1,16 @@
+function send(){
+    var arr= document.getElementsByTagName("input");
+    var name = arr[0].value;
+    var check4= arr[4].checked;
+    var check5= arr[5].checked;
+    var check6= arr[6].checked;
+    console.log(name);
+    if(check5||check6){
+        alert("Chúng tôi đã biết giới tính thật của bạn dề nghị bạn ");var a=7;return;
+    console.log(a)
+    }
+    else { 
+        alert("cảm ơn bạn đã tham gia bài khảo sát");var a=8;return
+    }
+    console.log(a)
+}
